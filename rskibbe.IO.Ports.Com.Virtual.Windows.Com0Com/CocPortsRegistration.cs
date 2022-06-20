51005ec6f0c3f96b10a37e606e4017667841e622
Robert Skibbe <skibbe.robert@web.de>
2022-06-20 11:08:59 +0200

    cleanup and work.. no time

---
+++ rskibbe.IO.Ports.Com.Virtual.Windows.Com0Com/CocPortsRegistration.cs
@@ -0,0 +1,35 @@
+﻿namespace rskibbe.IO.Ports.Com.Virtual.Windows.Com0Com;
+
+public class CocPortsRegistration : VirtualComPortRegistrationBase
+{
+
+    public int Id { get; set; }
+
+    public string IdentifierA => $"CNCA{Id}";
+
+    public string IdentifierB => $"CNCB{Id}";
+
+    public CocPortsRegistration()
+    {
+        Id = -1;
+    }
+
+    public CocPortsRegistration(int id)
+    {
+        Id = id;
+    }
+
+    public static CocPortsRegistration FromBinaryResponse(IEnumerable<string> lines)
+    {
+        var line = lines.First();
+        var lineValues = line.Split(" ");
+        var firstPart = lineValues[0];
+        firstPart.ExtractInt(out var id);
+        var registration = new CocPortsRegistration(id);
+        return registration;
+    }
+
+    public override string ToString()
+        => $"{Id}: {IdentifierA}->{ComPorts.NameA}, {IdentifierB}->{ComPorts.NameB}";
+
+}
